a9b92dea7cd5ba810f096c516f26edca5d3d98d1
MalevichDi2910 <malevicdiana@gmail.com>
2023-01-10 10:19:49 +0300

    hw 12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react'
+import React, {ChangeEvent, useEffect} from 'react'
 import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
 import {changeThemeId} from './bll/themeReducer'
+import {AppStoreType} from "../hw10/bll/store";
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
@@ -12,7 +13,11 @@ import {changeThemeId} from './bll/themeReducer'
 * 4 - передать пропсы в SuperSelect
 * */
 
-const themes = [
+export type themesPropsType = {
+    id: number, value: string
+}
+
+const themes: themesPropsType[] = [
     {id: 1, value: 'light'},
     {id: 2, value: 'blue'},
     {id: 3, value: 'dark'},
@@ -20,11 +25,18 @@ const themes = [
 
 const HW12 = () => {
     // взять ид темы из редакса
-    const themeId = 1
+    const themeId = useSelector<AppStoreType,number>(state => state.theme.themeId)
+    const dispatch = useDispatch()
 
-    const change = (id: any) => { // дописать функцию
-
-    }
+    const change = (id: number) => { //дописать функцию
+            if (id === 1) {
+                dispatch(changeThemeId(themes[0].id))
+            } else if (id === 2) {
+                dispatch(changeThemeId(2))
+            } else {
+                dispatch(changeThemeId(3))
+            }
+        }
 
     useEffect(() => {
         document.documentElement.dataset.theme = themeId + ''
@@ -41,7 +53,8 @@ const HW12 = () => {
                     id={'hw12-select-theme'}
                     className={s.select}
                     // сделать переключение тем
-
+                    options={themes}
+                    onChangeOption={change}
                 />
             </div>
         </div>
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
+type initStatePropsType = {
+    themeId: number
+}
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state: initStatePropsType = initState, action: changeThemeIdPropsType): initStatePropsType => { // fix any
     switch (action.type) {
         // дописать
+        case 'SET_THEME_ID':
+            return {...state, themeId: action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+
+export type changeThemeIdPropsType = {
+    type: 'SET_THEME_ID',
+    id: number
+}
+
+// export type changeThemeIdPropsType = ReturnType<typeof changeThemeId>
+
+export const changeThemeId = (id: number): changeThemeIdPropsType => ({ type: 'SET_THEME_ID', id } as const) // fix any
