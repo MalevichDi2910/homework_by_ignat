b3418c8704a27a97b36a23aac3005856cec90fa9
MalevichDi2910 <malevicdiana@gmail.com>
2023-03-23 13:13:32 +0300

    hw 15

---
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import down from './../../icons/down.png';
+import up from './../../icons/up.png';
+import arrows from './../../icons/arrows.png';
+
+const downIcon = down;
+const upIcon = up;
+const noneIcon = arrows;
+
+export type SuperSortPropsType = {
+    id?: string
+    sort: string
+    value: string
+    onChange: (newSort: string) => void
+}
+
+export const pureChange = (sort: string, down: string, up: string) => {
+
+    return sort === down ?
+        up : sort === up ?
+            '' : down
+}
+
+const SuperSort: React.FC<SuperSortPropsType> = (
+    {
+        sort, value, onChange, id = 'hw15',
+    }
+) => {
+    const up = '0' + value
+    const down = '1' + value
+
+    const onChangeCallback = () => {
+        onChange(pureChange(sort, down, up))
+    }
+
+    const icon = sort === down
+        ? downIcon
+        : sort === up
+            ? upIcon
+            : noneIcon
+
+    return (
+        <span
+            id={id + '-sort-' + value}
+            onClick={onChangeCallback}
+        >
+            <img
+                id={id + '-icon-' + sort}
+                src={icon}
+            />
+        </span>
+    )
+}
+
+export default SuperSort
